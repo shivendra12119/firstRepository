cdb3ea4394ac587413eff9a1b650443934ef792a
Shivendra Kadam <skadam@qualys.com>
2019-07-09 16:30:13 +0530

    Practice=added Suborder class

--- src/main/java/com/freefood/entity/SubOrder.java
+++ src/main/java/com/freefood/entity/SubOrder.java
@@ -1,6 +1,7 @@
 package com.freefood.entity;
 
 
+import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,7 +12,7 @@ import lombok.Data;
 
 @Entity
 @Data
-public class SubOrder {
+public class SubOrder implements Serializable{
   @Id
   @GeneratedValue(strategy=GenerationType.IDENTITY)
   private Integer id;
@@ -27,4 +28,25 @@ public class SubOrder {
   private Order order;
   
   private Long amount;
+
+  public SubOrder(Dish dish, Integer quantity, Long price) {
+    super();
+    this.dish = dish;
+    this.quantity = quantity;
+    this.amount = price*quantity;
+  }
+
+  public SubOrder() {
+    super();
+  }
+
+  @Override
+  public String toString() {
+    return "SubOrder [id=" + id + ", dish=" + dish.getName() + ", quantity=" + quantity + ", order=" + order.getId()
+        + ", amount=" + amount + "]";
+  }
+
+  
+  
+  
 }
